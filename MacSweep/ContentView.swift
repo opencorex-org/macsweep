4da256ab8e44217c9b62ec282b46c300d3fe6fb6
lahiruudayakumara <79270918+lahiruudayakumara@users.noreply.github.com>
2026-08-24 07:46:54 +0530

    Initial Commit

---
+++ MacSweep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacSweep
+//
+//  Created by LAHIRU UDAYAKUMARA on 2026-08-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacSweep/MacSweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSweepApp.swift
+//  MacSweep
+//
+//  Created by LAHIRU UDAYAKUMARA on 2026-08-24.
+//
+
+import SwiftUI
+
+@main
+struct MacSweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
